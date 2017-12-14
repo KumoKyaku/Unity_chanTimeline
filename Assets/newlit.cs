919408ebd7010debbed0d882212ca6862bdfe86d
云却不是雀 <479813005@qq.com>
2017-12-14 15:56:33 +0800

    no message

---
+++ Assets/newlit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class newlit : MonoBehaviour {
+    public Color test;
+    public Light l;
+    public Image i;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        l.color = test;
+        i.color = test;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Unity_chanTimeline
-动画片段测试
+动画片段测试，主要跑下流程。
+
+视频地址： 
+
+![image](/Image/封面.png)
+
